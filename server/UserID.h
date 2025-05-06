056e94e213dd164557504d99ed66e9f727b4788a
pythonIsBadExceptBlender <146817990+PythonIsBad@users.noreply.github.com>
2025-05-06 20:06:49 +0500

    Delete server directory

--- server/UserID.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "SFML/Graphics.hpp"
-#include <atomic>
-
-sf::RenderWindow* window;
-std::atomic <int> userID = 0;
-std::atomic <int> roomID = -1;
-std::atomic <bool> streaming = false;
